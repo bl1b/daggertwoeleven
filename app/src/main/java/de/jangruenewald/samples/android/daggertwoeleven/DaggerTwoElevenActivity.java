5e9aa3a5d7e577d9ea46c8c2b77ac8d2f5c1bc18
Jan Grünewald <jan.gruenewald84@googlemail.com>
2017-06-10 19:31:09 +0200

    added use-case: module injection

--- app/src/main/java/de/jangruenewald/samples/android/daggertwoeleven/DaggerTwoElevenActivity.java
+++ app/src/main/java/de/jangruenewald/samples/android/daggertwoeleven/DaggerTwoElevenActivity.java
@@ -9,6 +9,7 @@ import dagger.android.support.DaggerAppCompatActivity;
 import de.jangruenewald.samples.android.daggertwoeleven.cases.injectwithconstructor.DaggerTwoElevenActivityProvider;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  * Created by Jan-2 on 20.05.2017.
@@ -24,6 +25,10 @@ public class DaggerTwoElevenActivity extends DaggerAppCompatActivity {
     @Inject
     DaggerTwoElevenActivityProvider daggerTwoElevenActivityProvider;
 
+    @Inject
+    @Named("string_from_module")
+    String stringFromModule;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +37,13 @@ public class DaggerTwoElevenActivity extends DaggerAppCompatActivity {
         if (daggerTwoElevenActivityProvider != null) {
             ((TextView) findViewById(R.id.tv_string_from_constructor_injected)).setText(daggerTwoElevenActivityProvider.provideString());
         } else {
-            ((TextView) findViewById(R.id.tv_string_from_constructor_injected)).setText("Constructor-Injection failed.");
+            ((TextView) findViewById(R.id.tv_string_from_constructor_injected)).setText(R.string.constructor_injection_failed);
+        }
+
+        if (stringFromModule != null) {
+            ((TextView) findViewById(R.id.tv_string_from_module_injected)).setText(stringFromModule);
+        } else {
+            ((TextView) findViewById(R.id.tv_string_from_module_injected)).setText(R.string.module_injection_failed);
         }
     }
 }
--- app/src/main/java/de/jangruenewald/samples/android/daggertwoeleven/DaggerTwoElevenActivityModule.java
+++ app/src/main/java/de/jangruenewald/samples/android/daggertwoeleven/DaggerTwoElevenActivityModule.java
@@ -1,10 +1,18 @@
 package de.jangruenewald.samples.android.daggertwoeleven;
 
 import dagger.Module;
+import dagger.Provides;
+
+import javax.inject.Named;
 
 /**
  * Created by Jan-2 on 10.06.2017.
  */
 @Module
 public abstract class DaggerTwoElevenActivityModule {
+    @Provides
+    @Named("string_from_module")
+    static String provideStringFromModule() {
+        return "String from DaggerTwoElevenActivityModule";
+    }
 }
